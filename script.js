37c2372b50492f35bb0341e8e221c7753ce478c3
Christian Mann <christian-mann@utulsa.edu>
2022-04-11 03:40:02 -0400

    Functioning 2p breakout

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 function between(a,b,c) {
 	return a <= b && b < c;
 }
@@ -11,142 +12,246 @@ var H_BALL = 10;
 var W_BRICK = 20;
 var H_BRICK = 10;
 
-function main() {
-	var playing = true;
-	var paddle = document.getElementById("paddle");
-	var game = document.getElementById("game");
-	var message = document.getElementById("message");
-
-	var vpaddlex = 0;
-
-	var ppaddlex = 0;
-	var vballx;
-	var vbally;
-	var pballx;
-	var pbally;
-
-	function resetball() {
-		vballx = 2;
-		vbally = -2;
-		pballx = 225;
-		pbally = 400;
+class Physics {
+	constructor() {
+		this.px = 0;
+		this.py = 0;
+		this.vx = 0;
+		this.vy = 0;
+		this.width = 0;
+		this.height = 0;
+		this.elem = null;
+		this.team = 0;
 	}
-	resetball();
 
-	// add bricks
-	for (var row = 0; row < H_GAME/H_BRICK/2; row++) {
-		for (var col = 0; col < W_GAME/W_BRICK; col++) {
-			var brick = document.createElement("div");
-			brick.classList.add("brick");
-			brick.style.left = col * (W_BRICK);
-			brick.style.top = row * H_BRICK;
-			game.appendChild(brick);
+	update() {
+		this.px += this.vx;
+		this.py += this.vy;
+		if (this.px < 0) {
+			this.px = 0;
+			this.vx = Math.abs(this.vx);
+		}
+		if (this.px + this.width > W_GAME) {
+			this.px = W_GAME - this.width;
+			this.vx = -Math.abs(this.vx);
+		}
+		if (this.py < 0) {
+			this.py = 0;
+			this.vy = Math.abs(this.vy);
+		}
+		if (this.py + this.height > H_GAME) {
+			this.py = H_GAME - this.height;
+			this.vy = -Math.abs(this.vy);
 		}
 	}
 
-	// initial positions of paddle and ball
-	ppaddlex = 200;
+	draw() {
+		if (this.elem) {
+			this.elem.style.left = this.px + "px";
+			this.elem.style.top = this.py + "px";
+			this.elem.style.width = this.width + "px";
+			this.elem.style.height = this.height + "px";
+		}
+	}
+
+	collides(other) {
+		// other: Physics
+
+		var collides_x = (
+			this.px + this.width > other.px &&
+			this.px < other.px + other.width
+		);
+		var collides_y = (
+			this.py + this.height > other.py &&
+			this.py < other.py + other.height
+		);
+		return collides_x && collides_y;
+	}
+
+	center() {
+		return [this.px + this.width/2, this.py + this.height/2];
+	}
+}
+
+class Paddle extends Physics {
+	constructor() {
+		super();
+		this.width = W_PADDLE;
+		this.height = H_PADDLE;
+	}
+}
+
+class Ball extends Physics {
+	constructor() {
+		super();
+		this.width = W_BALL;
+		this.height = H_BALL;
+	}
+
+	draw() {
+		super.draw();
+		if (this.elem) {
+			this.elem.style.width = 0;
+			this.elem.style.height = 0;
+		}
+	}
+}
+
+class Brick extends Physics {
+	constructor() {
+		super();
+		this.width = W_BRICK;
+		this.height = H_BRICK;
+	}
+}
+
+function main() {
+	var playing = true;
+	var game = document.getElementById("game");
+
+	var balls = [new Ball(), new Ball()];
+	balls[0].elem = document.createElement("div");
+	balls[0].elem.classList.add("ball");
+	balls[0].elem.classList.add("team-0");
+	balls[0].px = 225;
+	balls[0].py = 400;
+	balls[0].vx = 2;
+	balls[0].vy = -2;
+	game.appendChild(balls[0].elem);
+
+	balls[1].elem = document.createElement("div");
+	balls[1].elem.classList.add("ball");
+	balls[1].elem.classList.add("team-1");
+	balls[1].team = 1;
+	balls[1].px = 225;
+	balls[1].py = 100;
+	balls[1].vx = -2;
+	balls[1].vy = 2;
+	game.appendChild(balls[1].elem);
+
+	var paddles = [new Paddle(), new Paddle()];
+	paddles[0].elem = document.createElement("div");
+	paddles[0].elem.classList.add("paddle");
+	paddles[0].elem.classList.add("team-0");
+	game.appendChild(paddles[0].elem);
+	paddles[0].px = 200;
+	paddles[0].py = H_GAME - H_PADDLE - 20;
+
+	paddles[1].elem = document.createElement("div");
+	paddles[1].elem.classList.add("paddle");
+	paddles[1].elem.classList.add("team-1");
+	paddles[1].team = 1;
+	game.appendChild(paddles[1].elem);
+	paddles[1].px = 210;
+	paddles[1].py = 20;
+
+	// add bricks
+	var bricks = [];
+	for (var row = 0; row < H_GAME/H_BRICK; row++) {
+		for (var col = 0; col < W_GAME/W_BRICK; col++) {
+			var brick = document.createElement("div");
+			brick.classList.add("brick");
+			game.appendChild(brick);
+
+			var myBrick = new Brick();
+			myBrick.elem = brick;
+			myBrick.px = col * W_BRICK;
+			myBrick.py = row * H_BRICK;
+			if (row >= H_GAME/H_BRICK/2) {
+				myBrick.team = 1;
+			}
+			brick.classList.add("team-"+myBrick.team);
+			myBrick.draw();
+			bricks.push(myBrick);
+		}
+	}
 
 
 	// key listener
 	window.onkeydown = (e) => {
 		if (e.key == "ArrowRight") {
-			vpaddlex = 3;
+			paddles[0].vx = 3;
 		} else if (e.key == "ArrowLeft") {
-			vpaddlex = -3;
+			paddles[0].vx = -3;
+		} else if (e.key == "d") {
+			paddles[1].vx = 3;
+		} else if (e.key == "a") {
+			paddles[1].vx = -3;
 		}
 	};
 
 	window.onkeyup = (e) => {
 		if (e.key == "ArrowRight" || e.key == "ArrowLeft") {
-			vpaddlex = 0;
+			paddles[0].vx = 0;
+		} else if (e.key == "a" || e.key == "d") {
+			paddles[1].vx = 0;
 		}
 	}
 	
 	// event loop
 	window.setInterval(() => {
-		// ball collision
-		if (pballx <= 0) {
-			// left wall
-			vballx = Math.abs(vballx);
-		}
-
-		if (pballx >= W_GAME - W_BALL) {
-			// right wall
-			vballx = -Math.abs(vballx);
-		}
-
-		if (pbally <= 0) {
-			// ceiling
-			vbally = Math.abs(vbally);
-		}
-
-		if (pbally >= H_GAME - H_BALL) {
-			// floor
-			resetball();
-		}
-
-		if (470 <= pbally && pbally < 490 &&
-			ppaddlex <= pballx && pballx < ppaddlex + W_PADDLE) {
-			// collision with paddle
-			vbally = -Math.abs(vbally);
-			// which quintet?
-			var quintet = (pballx - (ppaddlex + W_PADDLE/2)) / W_PADDLE * 5;
-			console.log('quintet', quintet);
-			vballx += quintet;
-			console.log('vballx', vballx);
+		for (var myPaddle of paddles) {
+			for (var myBall of balls) {
+				if (myBall.collides(myPaddle)) {
+					// collision with paddle
+					myBall.vy *= -1;
+					// which quintet?
+					var quintet = (myBall.px - (myPaddle.px + W_PADDLE/2)) / W_PADDLE * 5;
+					console.log('quintet', quintet);
+					myBall.vx += quintet;
+					console.log('vballx', myBall.vx);
+				}
+			}
 		}
 
 		// brick collisions
-		for (var brick of document.querySelectorAll(".brick")) {
-			pbrickx = Number(brick.style.left.slice(0,-2));
-			pbricky = Number(brick.style.top.slice(0,-2));
-			// top wall
-			if (Math.abs(pbally + 10 - pbricky) <= 2 &&
-				between(pbrickx, pballx, pbrickx + W_BRICK)) {
-				//debugger;
-				brick.parentElement.removeChild(brick);
-				vbally = -Math.abs(vbally);
+		for (var myBall of balls) {
+		for (var myBrick of bricks) {
+			if (myBrick.team != myBall.team) {
+				continue;
 			}
-			// bottom wall
-			if (Math.abs(pbally - (pbricky + 10)) <= 2 &&
-				between(pbrickx, pballx, pbrickx + W_BRICK)
-			) {
-				//debugger;
-				brick.parentElement.removeChild(brick);
-				vbally = Math.abs(vbally);
+			if (myBrick.collides(myBall)) {
+				// how?
+				var dx = myBall.center()[0] - myBrick.center()[0];
+				var dy = myBall.center()[1] - myBrick.center()[1];
+				dy = -dy; // upside down math
+				var angle = Math.atan2(dy, dx) * 180 / Math.PI;
+
+				if (-15 <= angle && angle < 15) {
+					// right
+					myBall.vx = Math.abs(myBall.vx);
+				} else if (15 <= angle && angle < 165) {
+					// top
+					myBall.vy = -Math.abs(myBall.vy);
+				} else if (-165 <= angle && angle < -15) {
+					// bottom
+					myBall.vy = Math.abs(myBall.vy);
+				} else {
+					// left
+					myBall.vx = -Math.abs(myBall.vx);
+				}
+				myBrick.elem.classList.remove("team-"+myBrick.team);
+				myBrick.team = 1 - myBall.team;
+				myBrick.elem.classList.add("team-"+myBrick.team);
+				break;
 			}
-			// left wall
-			if (
-				Math.abs(pballx - (pbrickx + W_BRICK)) <= 2 &&
-				between(pbricky, pbally, pbricky+10)
-			) {
-				brick.parentElement.removeChild(brick);
-				vballx *= -1;
-			}
-			if (
-				Math.abs(pballx + 10 - pbrickx) <= 2 &&
-				between(pbricky, pbally, pbricky+10)
-			) {
-				brick.parentElement.removeChild(brick);
-				vballx *= -1;
-			}
+		}
 		}
 
 		// ball movement
 		if (playing) {
-			pballx += vballx;
-			pbally += vbally;
-			ball.style.left = pballx + "px";
-			ball.style.top = pbally + "px";
+			for (myBall of balls) {
+				myBall.update();
+				myBall.draw();
+			}
 		}
 
 
 		// paddle
-		ppaddlex += vpaddlex;
-		if (ppaddlex < 0) ppaddlex = 0;
-		if (ppaddlex > W_GAME - W_PADDLE) ppaddlex = W_GAME - W_PADDLE;
-		paddle.style.left = ppaddlex + "px";
+		for (myPaddle of paddles) {
+			myPaddle.update();
+			myPaddle.draw();
+		}
 	}, 10);
 };
 
